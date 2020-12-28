37120762c92c5babcb887f8801901819100a45fb
Mohammad <mohammad.hussein@syngenta.com>
2020-12-27 18:17:38 -0600

    use env file

--- WMI-React-App-master/src/utils/axios.js
+++ WMI-React-App-master/src/utils/axios.js
@@ -4,8 +4,7 @@ export const headers = {
   'Content-Type': 'application/x-www-form-urlencoded'
 };
 
-let baseURL = `https://localhost:5001/api/`;
-
+let baseURL = `${process.env.REACT_APP_API_URL}`
 
 export const request = axios.create({
   baseURL
